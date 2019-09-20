9c4d805d89ecf7b28896445666fa03718611b790
Jack <ahmogo@yahoo.com>
2019-09-20 17:35:21 +0200

    Edited Server.js

--- src/net/Server.js
+++ src/net/Server.js
@@ -22,7 +22,7 @@ class Server extends EventEmitter {
         /**
          * @type {String}
          */
-        this.path = opts.path === undefined ? '/amf' : (opts.path || '/');
+        this.path = opts.path || '/';
 
         /**
          * XML crossdomain
