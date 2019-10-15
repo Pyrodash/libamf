b0aa05ecef2441169c27fe1b77e2577e746cc577
Jack <ahmogo@yahoo.com>
2019-10-15 18:33:23 +0200

    Optimized getPath function

--- src/net/Helper.js
+++ src/net/Helper.js
@@ -1,15 +1,16 @@
+const _module = require('module');
 const path = require('path');
 const fs = require('fs');
 
 module.exports = {
     isPacketArray: arr => arr && arr.constructor === Array && !(arr.find(item => !(item instanceof Packet))),
     getPath: function (myMdl) {
-        const _module = require('module');
+        const constructor = Object.getPrototypeOf(myMdl).constructor;
 
         for (var i in _module._cache) {
             const mdl = _module._cache[i];
 
-            if (Object.getPrototypeOf(myMdl).constructor === mdl.exports)
+            if (constructor === mdl.exports)
                 return mdl.filename.replace(path.extname(mdl.filename), '');
         }
     },
