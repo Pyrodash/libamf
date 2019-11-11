6825f063a70880e178241d4bfe6b3e15d6878648
Jack <me@jackie.pw>
2019-11-11 05:00:14 +0200

    Reworked class to use a ByteArray property instead of extending the ByteArray class

--- src/amf/AbstractAMF.js
+++ src/amf/AbstractAMF.js
@@ -1,16 +1,44 @@
 'use strict';
 
-const ByteArray = require('bytearray-node');
+const ByteArray = require('bytearray-node').Proxy;
 const AMF       = require('../');
 
-class AbstractAMF extends ByteArray {
-    constructor(core) {
-        super();
+const utils     = require('../utils/Utils');
+const methods   = new WeakMap();
 
+class AbstractAMF {
+    constructor(core) {
         /**
          * @type {AMF}
          */
         this.core = core || AMF;
+
+        if(!methods.get(this.constructor)) {
+            methods.set(this.constructor, utils.getAllMethods(this));
+        }
+    }
+
+    set byteArray(ba) {
+        if(ba) {
+            const locMethods = utils.getAllMethods(ba);
+            const originalMethods = methods.get(this.constructor);
+
+            for(var i in locMethods) {
+                var methodName = locMethods[i];
+                
+                if(originalMethods.includes(methodName)) {
+                    this['super_' + methodName] = ba[methodName].bind(ba);
+                } else {
+                    this[methodName] = ba[methodName].bind(ba);
+                }
+            }
+        }
+
+        this.__byteArray = ba;
+    }
+
+    get byteArray() {
+        return this.__byteArray;
     }
 
     /**
