0b4e4177f8eacae1a8f1a4a7510d117827561604
Jack <me@jackie.pw>
2019-11-11 04:58:24 +0200

    Removed async readFile & added a method to get all methods

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -33,4 +33,34 @@ exports.isAssociativeArray = data => data instanceof Array && Object.keys(data).
 
 exports.getConstructor = cls => cls.constructor.name !== 'Function' ? cls.constructor : cls;
 
-exports.readFileAsync = util.promisify(fs.readFile);
+exports.getAllMethods = function (obj) {
+    function isGetter(obj, prop) {
+        return !!obj.__lookupGetter__(prop)
+    }
+
+    let keys = []
+    let topObject = obj
+
+    const onlyOriginalMethods = (p, i, arr) => {
+        return !isGetter(topObject, p) &&
+            typeof topObject[p] === 'function' &&
+            p !== 'constructor' &&
+            (i === 0 || p !== arr[i - 1]) &&
+            keys.indexOf(p) === -1
+    }
+
+    do {
+        const l = Object.getOwnPropertyNames(obj)
+            .sort()
+            .filter(onlyOriginalMethods)
+        keys = keys.concat(l)
+
+        // walk-up the prototype chain
+        obj = Object.getPrototypeOf(obj)
+    } while (
+        // not the the Object prototype methods (hasOwnProperty, etc...)
+        obj && Object.getPrototypeOf(obj)
+    )
+
+    return keys
+};
